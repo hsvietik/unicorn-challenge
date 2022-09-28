cc835a3b584add0978fcb8db6e1e39c013678a0f
hsvietik <hsvietik@gmail.com>
2022-09-28 23:22:53 +0100

    results page added

--- js/quiz.js
+++ js/quiz.js
@@ -3,3 +3,9 @@ function nextPage(previous, newPage) {
   document.getElementById(previous).style.display = "none";
   document.getElementById(newPage).style.display = "inline-block";
 }
+function colourChanges() {
+  document.getElementById("container").style.backgroundColor =
+    "var(--background-color-blue)";
+  document.getElementById("restart").style.backgroundColor =
+    "var(--color-blue)";
+}
