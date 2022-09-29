136892aaaa8307073369eadc4d99a980f48a2b0d
hsvietik <hsvietik@gmail.com>
2022-09-29 14:31:01 +0100

    name and colour changing  added

--- js/quiz.js
+++ js/quiz.js
@@ -3,9 +3,18 @@ function nextPage(previous, newPage) {
   document.getElementById(previous).style.display = "none";
   document.getElementById(newPage).style.display = "inline-block";
 }
-function colourChanges() {
-  document.getElementById("container").style.backgroundColor =
-    "var(--background-color-blue)";
-  document.getElementById("restart").style.backgroundColor =
-    "var(--color-blue)";
+//Changing colours of background and buttons
+function colourChanges(background, buttons) {
+  document.getElementById("container").style.backgroundColor = background;
+  document.querySelectorAll(".button").style.backgroundColor = buttons;
+}
+//
+// const menuItemsByClass = document.querySelectorAll(".button");
+// console.log(menuItemsByClass);
+//Using Name of player in questions
+function playersName() {
+  const player = document.forms["players-name-input"]["name"].value;
+  document.getElementById("message").style.display = "block";
+  document.getElementById("name-text").innerHTML =
+    "Nice to meet you " + player + "!";
 }
